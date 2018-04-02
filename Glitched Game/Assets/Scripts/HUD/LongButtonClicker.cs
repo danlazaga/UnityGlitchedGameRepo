4a7b016be09d5bdef8c7559b4898aacbbd1de4dc
Cyrille Dan Lazaga <danlazaga@gmail.com>
2018-04-03 02:56:40 +0800

    LongButtonClicker Added

---
+++ Glitched Game/Assets/Scripts/HUD/LongButtonClicker.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class LongButtonClicker : MonoBehaviour, IButtonInteract, IPointerDownHandler, IPointerUpHandler
+{
+
+#region Variables
+	public event Action OnInteract = delegate { };
+	private bool pointerDown;
+	private float pointerDownTimer;
+	private Image baseImage;
+	[SerializeField] private float requiredHoldTime;
+	[SerializeField] private Image fillImage;
+#endregion
+
+#region Unity Methods
+
+	void Start()
+	{
+		baseImage = GetComponent<Image>();
+		fillImage.sprite = baseImage.sprite;
+	}
+
+	void Update()
+	{
+		if (pointerDown)
+		{
+			pointerDownTimer += Time.deltaTime;
+
+			if (pointerDownTimer >= requiredHoldTime)
+			{
+				OnClick();
+				Reset();
+			}
+
+			fillImage.fillAmount = pointerDownTimer / requiredHoldTime;
+		}
+	}
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		pointerDown = true;
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		pointerDown = false;
+	}
+#endregion
+
+	public void OnClick()
+	{
+		if (OnInteract != null)OnInteract();
+	}
+
+	void Reset()
+	{
+		pointerDownTimer = 0;
+		fillImage.fillAmount = pointerDownTimer / requiredHoldTime;
+	}
+}
