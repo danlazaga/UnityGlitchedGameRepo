9c92cef5129eed264f14cb977b4b1df3d9eb7aca
Joshua James Resurreccion <joshuajamesresurreccion@gmail.com>
2018-04-07 18:11:37 +0800

    Added ModifiedNetworkLobbyManager, Added UI for Lobby.

---
+++ Glitched Game/Assets/Scripts/Lobby/ModifiedNetworkLobbyManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ModifiedNetworkLobbyManager : NetworkLobbyManager
+{
+	[Space(10)]
+	[SerializeField] string sceneName;
+
+	private static ModifiedNetworkLobbyManager instance;
+
+	public static ModifiedNetworkLobbyManager Instance
+	{
+		get
+		{
+			return instance;
+		}
+	}
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	public void StartHostModified()
+	{
+		StartHost();
+	}
+
+	public override void OnStartHost()
+	{
+		base.OnStartHost();
+
+		Debug.Log("Host Started!");
+	}
+
+	public void JoinGame(string networkAdressString)
+	{
+		networkAddress = networkAdressString;
+
+		StartClient();
+	}
+
+	public override void OnStartClient(NetworkClient client)
+	{
+		base.OnStartClient(client);
+
+		Debug.Log("Connected!");
+	}
+
+	public void ChangeScene()
+	{
+		ServerChangeScene(sceneName);
+	}
+}
