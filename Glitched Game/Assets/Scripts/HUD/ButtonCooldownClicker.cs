2faf28a2fe728056d4d010ecd6ad070b49090a53
Cyrille Dan Lazaga <danlazaga@gmail.com>
2018-04-11 22:43:45 +0800

    Mobile Input
    -working fire button reflects on PlayerInput.cs
    -working cooldownButton  reflects on PlayerInput.cs

---
+++ Glitched Game/Assets/Scripts/HUD/ButtonCooldownClicker.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonCooldownClicker : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+#region Variables
+	[SerializeField] Image darkMask;
+	[SerializeField] Text coolDownTextDisplay;
+	[SerializeField] float coolDownDuration;
+	private float nextReadyTime;
+	private float coolDownTimeLeft;
+	bool pointerDown;
+	public event Action OnInteract;
+	public event Action OnInteractUp;
+
+#endregion
+
+#region Unity Methods
+	void Update()
+	{
+		bool coolDownComplete = (Time.time > nextReadyTime);
+
+		if (coolDownComplete)
+		{
+			AbilityReady();
+			if (pointerDown)
+			{
+				ButtonTriggered();
+			}
+		}
+		else
+		{
+			CoolDown();
+		}
+	}
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		pointerDown = true;
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		pointerDown = false;
+		if(OnInteractUp != null) OnInteractUp();
+	}
+#endregion
+
+	void AbilityReady()
+	{
+		coolDownTextDisplay.enabled = false;
+		darkMask.enabled = false;
+	}
+
+	void CoolDown()
+	{
+		pointerDown = false;
+		coolDownTimeLeft -= Time.deltaTime;
+		float roundedCd = Mathf.Round(coolDownTimeLeft);
+		coolDownTextDisplay.text = roundedCd.ToString();
+		darkMask.fillAmount = (coolDownTimeLeft / coolDownDuration);
+	}
+
+	private void ButtonTriggered()
+	{
+		nextReadyTime = coolDownDuration + Time.time;
+		coolDownTimeLeft = coolDownDuration;
+		darkMask.enabled = true;
+		coolDownTextDisplay.enabled = true;
+
+		OnClick();
+	}
+
+	public void OnClick()
+	{
+		if(OnInteract!= null) OnInteract();
+	}
+}
