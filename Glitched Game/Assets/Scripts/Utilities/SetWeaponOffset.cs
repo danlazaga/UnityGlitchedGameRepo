6bd11addfb65580e196af70e439fa6930d1cfc4f
iACADEMY-VRPC <igda-iacademy@iacademy.edu.ph>
2018-05-02 16:15:44 +0800

    Steam VR SDK

---
+++ Glitched Game/Assets/Scripts/Utilities/SetWeaponOffset.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetWeaponOffset : MonoBehaviour {
+
+public Quaternion weaponOffset;
+
+void OnEnable()
+{
+	this.transform.rotation = weaponOffset;
+}
+
+
+}
