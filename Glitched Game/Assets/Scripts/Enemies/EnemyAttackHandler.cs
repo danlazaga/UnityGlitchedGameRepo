62dce31db69bcff00b748cd9b5a8e4ead6194934
Basil Benitez <benitezbasil@gmail.com>
2018-05-08 17:13:23 +0800

    Enemy Hit

---
+++ Glitched Game/Assets/Scripts/Enemies/EnemyAttackHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackHandler : MonoBehaviour {
+
+    public Collider attackCollider;
+
+	public void OpenAttackCollider()
+    {
+        attackCollider.enabled = true;
+    }
+    public void CloseAttackCollider()
+    {
+        attackCollider.enabled = false;
+    }
+}
