562f93096388c8d5947559d805b9e7e0661239d9
iACADEMY-VRPC <igda-iacademy@iacademy.edu.ph>
2018-05-08 20:26:37 +0800

    Mesh renderer feedback

---
+++ Glitched Game/Assets/Scripts/Modular/ActorMesh.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorMesh : MonoBehaviour
+{
+	[SerializeField] Renderer rendererActor;
+	WaitForSeconds delay1 = new WaitForSeconds(0.7f);
+
+	private readonly int materialGlow = Shader.PropertyToID("_GlowToggle");
+	private readonly int materialHit = Shader.PropertyToID("_DamageorStun");
+
+	private void Awake()
+	{
+		if (GetComponentInParent<IHealthHandler>()!= null)
+		{
+			GetComponentInParent<IHealthHandler>().OnHPPctChanged += HandleHitMesh;
+		}
+
+		// if (GetComponentInParent<IStunHandler>()!= null)
+		// {
+		// 	GetComponent<IStunHandler>().OnStun += HandleStunMesh;
+		// }
+	}
+
+	private void OnDisable()
+	{
+		rendererActor.material.SetFloat(materialGlow, 0);
+	}
+
+	private void HandleHitMesh(float value)
+	{
+		StartCoroutine(PlayHitMesh());
+	}
+
+	private void HandleStunMesh(float value)
+	{
+
+	}
+
+	IEnumerator PlayHitMesh()
+	{
+		rendererActor.material.SetFloat(materialHit, 0);
+		rendererActor.material.SetFloat(materialGlow, 1);
+		yield return delay1;
+		rendererActor.material.SetFloat(materialGlow, 0);
+	}
+}
