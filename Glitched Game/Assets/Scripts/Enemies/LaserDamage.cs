adb8c854318e17454b1c3a61d303ec4fc8b5ebf0
Basil Benitez <benitezbasil@gmail.com>
2018-05-11 20:43:32 +0800

    no message

---
+++ Glitched Game/Assets/Scripts/Enemies/LaserDamage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserDamage : MonoBehaviour {
+
+    [SerializeField] private float laserDamage;
+
+    private const int PLAYER_MASK = 9;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+
+        }
+    }
+}
---
+++ Glitched Game/Assets/Scripts/Enemies/MissileDamage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileDamage : MonoBehaviour {
+
+    [SerializeField] private float missileDamage;
+
+    private const int PLAYER_MASK = 9;
+    private Vector3 startPos;
+
+    private void OnEnable()
+    {
+        startPos = this.transform.position;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == 9)
+        {
+
+        }
+    }
+
+    private void OnDisable()
+    {
+        this.transform.position = startPos;
+    }
+}
