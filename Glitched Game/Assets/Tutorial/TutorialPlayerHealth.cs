b5cad74a5f3fc1fc4cb1451cdc4874d321bc4f9c
Joshua James Resurreccion <joshuajamesresurreccion@gmail.com>
2018-05-15 22:05:34 +0800

    no message

---
+++ Glitched Game/Assets/Tutorial/TutorialPlayerHealth.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+
+public class TutorialPlayerHealth : MonoBehaviour, IHealthHandler
+{
+
+#region Variables
+	[SerializeField] float maxHealth = 100;
+	[SerializeField] float health;
+	public event Action<float> OnHPPctChanged = delegate(float f) { };
+	public event Action OnDied = delegate { };
+
+#endregion
+
+#region Unity Methods
+	void OnEnable()
+	{
+		health = maxHealth;
+	}
+#endregion
+	public void TakeDamage(float amount)
+	{
+		bool died = false;
+
+		if (amount <= 0)
+			throw new ArgumentOutOfRangeException("Invalid Damage amount specified: " + amount);
+
+		if (health <= 0)
+			return;
+
+		health -= amount;
+
+		died = health <= 0;
+
+		RpcTakeDamage(died);
+	}
+	void RpcTakeDamage(bool died)
+	{
+		OnHPPctChanged(health);
+
+		PlayerHUD.Instance.FlashDamageEffect();
+
+		if (died)
+			OnDied();
+	}
+
+	void OnHealthChanged(float value)
+	{
+		health = value;
+
+		PlayerHUD.Instance.SetHealth(value);
+	}
+}
---
+++ Glitched Game/Assets/Tutorial/TutorialPlayerSetup.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TutorialPlayerSetup : MonoBehaviour
+{
+#region Variables
+	GameObject mainCamera;
+	[SerializeField] GameObject playerCamera;
+	[SerializeField] GameObject rightHand;
+	[SerializeField] GameObject leftHand;
+
+#endregion
+
+#region Unity Methods
+
+	void Start()
+	{
+		mainCamera = Camera.main.gameObject;
+
+		EnablePlayer();
+	}
+
+#endregion
+
+	void EnablePlayer()
+	{
+		mainCamera.SetActive(false);
+		playerCamera.SetActive(true);
+		rightHand.SetActive(true);
+		leftHand.SetActive(true);
+	}
+}
