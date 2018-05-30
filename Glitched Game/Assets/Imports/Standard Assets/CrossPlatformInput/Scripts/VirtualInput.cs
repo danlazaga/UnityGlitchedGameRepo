6f21b3ac73dda96a70eb66caca23e8b587ede434
Cyrille Dan Lazaga <danlazaga@gmail.com>
2018-05-30 22:58:05 +0800

    Mobile Fix when rejoining game

--- Glitched Game/Assets/Imports/Standard Assets/CrossPlatformInput/Scripts/VirtualInput.cs
+++ Glitched Game/Assets/Imports/Standard Assets/CrossPlatformInput/Scripts/VirtualInput.cs
@@ -2,22 +2,19 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 namespace UnityStandardAssets.CrossPlatformInput
 {
     public abstract class VirtualInput
     {
         public Vector3 virtualMousePosition { get; private set; }
-        
-        
+
         protected Dictionary<string, CrossPlatformInputManager.VirtualAxis> m_VirtualAxes =
             new Dictionary<string, CrossPlatformInputManager.VirtualAxis>();
-            // Dictionary to store the name relating to the virtual axes
+        // Dictionary to store the name relating to the virtual axes
         protected Dictionary<string, CrossPlatformInputManager.VirtualButton> m_VirtualButtons =
             new Dictionary<string, CrossPlatformInputManager.VirtualButton>();
         protected List<string> m_AlwaysUseVirtual = new List<string>();
-            // list of the axis and button names that have been flagged to always use a virtual axis or button
-        
+        // list of the axis and button names that have been flagged to always use a virtual axis or button
 
         public bool AxisExists(string name)
         {
@@ -29,13 +26,14 @@ namespace UnityStandardAssets.CrossPlatformInput
             return m_VirtualButtons.ContainsKey(name);
         }
 
-
         public void RegisterVirtualAxis(CrossPlatformInputManager.VirtualAxis axis)
         {
             // check if we already have an axis with that name and log and error if we do
             if (m_VirtualAxes.ContainsKey(axis.name))
             {
-                Debug.LogError("There is already a virtual axis named " + axis.name + " registered.");
+                // Debug.LogError("There is already a virtual axis named " + axis.name + " registered.");
+                UnRegisterVirtualAxis(axis.name);
+                RegisterVirtualAxis(axis);
             }
             else
             {
@@ -49,7 +47,6 @@ namespace UnityStandardAssets.CrossPlatformInput
                 }
             }
         }
-
 
         public void RegisterVirtualButton(CrossPlatformInputManager.VirtualButton button)
         {
@@ -71,7 +68,6 @@ namespace UnityStandardAssets.CrossPlatformInput
             }
         }
 
-
         public void UnRegisterVirtualAxis(string name)
         {
             // if we have an axis with that name then remove it from our dictionary of registered axes
@@ -80,7 +76,6 @@ namespace UnityStandardAssets.CrossPlatformInput
                 m_VirtualAxes.Remove(name);
             }
         }
-
 
         public void UnRegisterVirtualButton(string name)
         {
@@ -91,34 +86,29 @@ namespace UnityStandardAssets.CrossPlatformInput
             }
         }
 
-
         // returns a reference to a named virtual axis if it exists otherwise null
         public CrossPlatformInputManager.VirtualAxis VirtualAxisReference(string name)
         {
-            return m_VirtualAxes.ContainsKey(name) ? m_VirtualAxes[name] : null;
+            return m_VirtualAxes.ContainsKey(name)? m_VirtualAxes[name] : null;
         }
-
 
         public void SetVirtualMousePositionX(float f)
         {
             virtualMousePosition = new Vector3(f, virtualMousePosition.y, virtualMousePosition.z);
         }
 
-
         public void SetVirtualMousePositionY(float f)
         {
             virtualMousePosition = new Vector3(virtualMousePosition.x, f, virtualMousePosition.z);
         }
-
 
         public void SetVirtualMousePositionZ(float f)
         {
             virtualMousePosition = new Vector3(virtualMousePosition.x, virtualMousePosition.y, f);
         }
 
-
         public abstract float GetAxis(string name, bool raw);
-        
+
         public abstract bool GetButton(string name);
         public abstract bool GetButtonDown(string name);
         public abstract bool GetButtonUp(string name);
